702965c1446a88ec4a765040aa890ff727abd39b
PreetiSharma15 <pwhitehat6@gmail.com>
2021-08-12 21:07:10 +0530

    Updated

--- DownloadAndMove.py
+++ DownloadAndMove.py
@@ -8,60 +8,62 @@ import shutil
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-from_dir = "C:/Users/ADMIN/Downloads"
-to_dir = "C:/WhiteHatJr/dowanloadedimages"
+from_dir = "C:/Users/preet/Downloads"              # Add the path of you "Downloads" folder.
+to_dir = "C:/Users/preet/Desktop/Downloaded_Files" #Create "Document_Files" folder in your Desktop and update the path accordingly.
 
 
 dir_tree = {
     "Image_Files": ['.jpg', '.jpeg', '.png', '.gif', '.jfif'],
     "Video_Files": ['.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.mp4', '.m4p', '.m4v', '.avi', '.mov'],
-    "Document_Files": ['.ppt', '.xls', '.csv', '.pdf', '.txt'],
+    "Document_Files": ['.ppt', '.xls', '.xlsx' '.csv', '.pdf', '.txt'],
     "Setup_Files": ['.exe', '.bin', '.cmd', '.msi', '.dmg']
 }
 
+# Event Hanlder Class
 
 class FileMovementHandler(FileSystemEventHandler):
 
     def on_created(self, event):
+
+        name, extension = os.path.splitext(event.src_path)
        
-        name, extension = os.path.splitext(event.src_path)       
         time.sleep(1)
-        print(name)
 
         for key, value in dir_tree.items():
             time.sleep(1)
-            print(key)
-            print(value)
 
             if extension in value:
-                print("Mathces") 
+
                 file_name = os.path.basename(event.src_path)
+               
                 print("Downloaded " + file_name)
-                
+
                 path1 = from_dir + '/' + file_name
                 path2 = to_dir + '/' + key
                 path3 = to_dir + '/' + key + '/' + file_name
-                print("path1 :"+path1)
-                print("path3 :"+path3)
-                if os.path.exists(path2):
-                    print("Moving " + file_name + ".....")
 
-                    # Move from path1 ---> path3
+                if os.path.exists(path2):
+
+                    print("Directory Exists...")
+                    print("Moving " + file_name + "....")
                     shutil.move(path1, path3)
+                    time.sleep(1)
 
                 else:
+                    print("Making Directory...")
                     os.makedirs(path2)
-                    print("Moving " + file_name + ".....")
+                    print("Moving " + file_name + "....")
                     shutil.move(path1, path3)
+                    time.sleep(1)
 
-# Initialize event handler
+# Initialize Event Handler Class
 event_handler = FileMovementHandler()
 
 # Initialize Observer
 observer = Observer()
 
+# Schedule the Observer
 observer.schedule(event_handler, from_dir, recursive=True)
-
 
 # Start the Observer
 observer.start()
@@ -71,7 +73,6 @@ try:
         time.sleep(2)
         print("running...")
 except KeyboardInterrupt:
-    print("stopped")
+    print("stopped!")
     observer.stop()
 
-observer.join()
